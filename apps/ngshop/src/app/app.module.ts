e0092ea12bb689824fe267336a8514f341de5fae
Ragnar Öst <ragnar@patronum.se>
2021-12-04 17:07:43 +0100

    added new scss structure for styleing, added primflex

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -8,6 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { UiModule } from '@frontend/ui';
+import { AccordionModule } from 'primeng/accordion';
 
 const routes: Routes = [
     {
@@ -27,7 +29,13 @@ const routes: Routes = [
         HeaderComponent,
         FooterComponent
     ],
-    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes),
+        UiModule,
+        AccordionModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
